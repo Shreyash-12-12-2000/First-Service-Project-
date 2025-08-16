97736fc46f872145b1db0847cac63787a4ab4627
shreyash-12-12-2000 <shreyashchaple12@gmail.com>
2025-08-16 16:05:33 +0530

    database link change for admin

--- UD admin/src/api/axios.tsx
+++ UD admin/src/api/axios.tsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  // baseURL: 'http://localhost:3000/api',
+   baseURL: 'https://ud-u86f.onrender.com/api', // ✅ Use your backend http 
    withCredentials: true
 });
 
