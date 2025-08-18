4007f7b005bbe2fa18f27ebeacc736ca56e398d5
shreyash-12-12-2000 <shreyashchaple12@gmail.com>
2025-08-18 16:42:30 +0530

    Add cookie-parser middleware and fix CORS for cookie support

--- UD server/app.js
+++ UD server/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 require('dotenv').config();
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
 
 // Routes
 const userRoutes = require('./routes/user.routes');
