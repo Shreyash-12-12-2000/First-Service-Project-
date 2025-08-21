6b80dd0c365127c32b6b80b9a2de47ccda85c920
shreyash-12-12-2000 <shreyashchaple12@gmail.com>
2025-08-21 17:40:40 +0530

    main domain on server

--- UD server/app.js
+++ UD server/app.js
@@ -20,7 +20,9 @@ app.use(cors({
   origin: function (origin, callback) {
     const allowedOrigins = [
       "http://localhost:5173",
-      "http://localhost:8080"
+      "http://localhost:8080",
+      "https://umbrelladesk.netlify.app",
+      "https://umbrelladesk.com",
     ];
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
